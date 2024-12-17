25778db1ef9da2ef58d38a2ffb83650b0bf999e5
romany10203 <romanym2020@gmail.com>
2024-12-17 21:12:05 +0200

    adding the search bar, filter, and the date picker in attendance layout

--- src/modules/hhrr/attendance/layouts/Attendance.tsx
+++ src/modules/hhrr/attendance/layouts/Attendance.tsx
@@ -1,4 +1,10 @@
+import { Box } from '@mui/material'
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import FilterBtn from 'modules/core/components/FilterBtn'
+import { Flex } from 'modules/core/components/flex'
 import { Heading, Page, HeadingActions } from 'modules/core/components/page'
+import { Search } from 'modules/core/components/Search'
 import React from 'react'
 
 export default function Attendance() {
@@ -15,6 +21,17 @@ export default function Attendance() {
           mainModal={null}
         />
       </Heading>
+      <Flex gap={5}>
+        {/* <SearchInput tableSearch/> */}
+        <Search/>
+        <FilterBtn/>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker/>
+        </LocalizationProvider>
+      </Flex>
+      <Box>
+
+      </Box>
     </Page>
   )
 }
